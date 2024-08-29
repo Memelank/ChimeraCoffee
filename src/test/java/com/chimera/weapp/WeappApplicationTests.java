d4d116346a25b67d162c46a0cd1b00f8adfd039d
memelank <space_bebop@163.com>
2024-08-29 20:57:40 +0800

    unit

---
+++ src/test/java/com/chimera/weapp/WeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimera.weapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
