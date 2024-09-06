531692893f42154546502320594d59d70649cc70
memelank <space_bebop@163.com>
2024-09-06 13:12:41 +0800

    Finish：商品表中的图片上传

--- src/main/java/com/chimera/weapp/controller/ProductController.java
+++ src/main/java/com/chimera/weapp/controller/ProductController.java
@@ -3,14 +3,22 @@ package com.chimera.weapp.controller;
 import com.chimera.weapp.entity.Product;
 import com.chimera.weapp.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    @Value("${file.upload-dir}")
+    private String uploadDirectory;
+
     @Autowired
     private ProductRepository repository;
 
@@ -24,9 +32,28 @@ public class ProductController {
         return ResponseEntity.ok(repository.save(entity));
     }
 
+//    @PostMapping
+//    public ResponseEntity<Product> createEntity(@RequestBody Product entity) {
+//        return ResponseEntity.ok(repository.save(entity));
+//    }
 
-    @PostMapping
-    public ResponseEntity<Product> createEntity(@RequestBody Product entity) {
+    @PostMapping(consumes = {"multipart/form-data"})
+    public ResponseEntity<Product> createEntity(
+            @RequestPart("product") Product entity,
+            @RequestPart("image") MultipartFile imageFile) throws IOException {
+
+        // 上传文件到服务器
+        if (!imageFile.isEmpty()) {
+            String filename = imageFile.getOriginalFilename();
+            File destinationFile = new File(uploadDirectory + filename);
+            imageFile.transferTo(destinationFile);
+
+            // 将上传后的文件路径或URL存储到imgURL中
+            entity.setImgURL(filename);  // 可以根据实际情况调整URL前缀
+        }
+
+        // 保存产品信息到数据库
         return ResponseEntity.ok(repository.save(entity));
     }
+
 }
