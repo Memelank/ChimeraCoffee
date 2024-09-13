30a1dbb5fa42037cf644ab43e5c5d60e3b7a9fcd
luojian <space_bebop@163.com>
2024-09-13 16:06:42 +0800

    feat: 前后端CORS配置

---
+++ src/main/java/com/chimera/weapp/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.chimera.weapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 允许的前端地址
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
