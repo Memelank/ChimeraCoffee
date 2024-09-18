fb998b4188529c6bc2ff6f73fe0b4e4bb10fd7ff
luojian <space_bebop@163.com>
2024-09-18 16:51:51 +0800

    feat: 增加updateProductOption

--- src/main/java/com/chimera/weapp/controller/ProductOptionController.java
+++ src/main/java/com/chimera/weapp/controller/ProductOptionController.java
@@ -2,10 +2,12 @@ package com.chimera.weapp.controller;
 
 import com.chimera.weapp.annotation.LoginRequired;
 import com.chimera.weapp.annotation.RolesAllow;
+import com.chimera.weapp.entity.Product;
 import com.chimera.weapp.entity.ProductOption;
 import com.chimera.weapp.enums.RoleEnum;
 import com.chimera.weapp.repository.ProductOptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +18,20 @@ public class ProductOptionController {
     @Autowired
     private ProductOptionRepository repository;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @LoginRequired
     @RolesAllow(RoleEnum.ADMIN)
     public ResponseEntity<ProductOption> createProductOption(@RequestBody ProductOption entity) {
         return ResponseEntity.ok(repository.save(entity));
     }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @LoginRequired
+    @RolesAllow(RoleEnum.ADMIN)
+    public ResponseEntity<ProductOption> updateProductOption(@RequestBody ProductOption entity) {
+        return ResponseEntity.ok(repository.save(entity));
+    }
+
     @GetMapping
     public List<ProductOption> getAllProductOptions() {
         List<ProductOption> all = repository.findAll();
