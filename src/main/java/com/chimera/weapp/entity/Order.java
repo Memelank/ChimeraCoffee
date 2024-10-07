ab45f91b12d8b59072eb5d66b446cab80d0d8837
az0bottle <1553359439@qq.com>
2024-10-07 21:06:56 +0800

    docs:订单collection的swagger说明

--- src/main/java/com/chimera/weapp/entity/Order.java
+++ src/main/java/com/chimera/weapp/entity/Order.java
@@ -2,6 +2,8 @@ package com.chimera.weapp.entity;
 
 import com.chimera.weapp.vo.OrderItem;
 import com.chimera.weapp.vo.DeliveryInfo;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,20 +21,28 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "就是订单呀")
 public class Order {
     @Id
     private ObjectId id;
     private ObjectId userId;
     private String state;
+    @Schema(description = "如\"未认证为学生身份的用户业务\"")
     private String customerType;
     private String scene;
+    @Schema(description = "定时达配送信息，暂不用填")
     private DeliveryInfo deliveryInfo;
+    @ArraySchema(arraySchema = @Schema(description = "订单所含商品列表", example = "[example1,example2...]"),
+            schema = @Schema(description = "订单其中的一个商品"))
     private List<OrderItem> items;
     private int orderNum;
+    @Schema(description = "顾客备注")
     private String remark;
     private String merchantNote;
+    @Schema(description = "前端先计算一个，后端会check")
     private int totalPrice;
 
-    @CreatedDate  // 自动填充创建时间
+    @CreatedDate
+    @Schema(description = "自动填充创建时间")
     private Date createdAt;
 }
