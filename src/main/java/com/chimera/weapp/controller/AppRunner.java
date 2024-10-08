fdeb87eedfaaaaf901723eff29b70313bc8b58ac
memelank <space_bebop@163.com>
2024-10-08 16:38:49 +0800

    fix: 同步测试服务

--- src/main/java/com/chimera/weapp/controller/AppRunner.java
+++ src/main/java/com/chimera/weapp/controller/AppRunner.java
@@ -10,12 +10,14 @@ import org.springframework.stereotype.Component;
 public class AppRunner implements CommandLineRunner {
 
 	@Autowired
-	private TestServiceDev userService;
+	private TestServiceDev testService;
 
 	@Override
 	public void run(String... args) throws Exception {
 //		// 调用创建新用户的方法
+
 //		userService.createNewUser();
+//		testService.createNewProcessorMap();
 
 
 	}
