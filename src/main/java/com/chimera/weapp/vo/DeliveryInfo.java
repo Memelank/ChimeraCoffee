59cb5a27d2ea15af2289edbdfd2df18cf1a4144c
memelank <space_bebop@163.com>
2024-11-07 00:07:06 +0800

    docs: 增加了对于时间的说明

--- src/main/java/com/chimera/weapp/vo/DeliveryInfo.java
+++ src/main/java/com/chimera/weapp/vo/DeliveryInfo.java
@@ -16,6 +16,6 @@ public class DeliveryInfo {
     private String school;
     @Schema(description = "绑定到具体订单实例的已选地址")
     private String address;
-    @Schema(description = "绑定到具体订单实例的已选时间")
+    @Schema(description = "绑定到具体订单实例的已选时间，可以是当天也可以是第二天。")
     private Date time;
 }
