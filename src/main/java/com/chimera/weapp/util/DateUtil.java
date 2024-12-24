649d016a4a79e0492742cdbe4ec271c43d255d4a
az0bottle <1553359439@qq.com>
2024-12-25 00:19:31 +0800

    fix:解决发通知时data.time2.value invalid问题

--- src/main/java/com/chimera/weapp/util/DateUtil.java
+++ src/main/java/com/chimera/weapp/util/DateUtil.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import java.util.TimeZone;
 public class DateUtil {
     // 定义日期格式
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
     public static String formatDate(Date date) {
         // 设置为东八区（中国标准时间）
         sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
 
         // 格式化日期
         return sdf.format(date);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(formatDate(new Date()));
     }
 }
